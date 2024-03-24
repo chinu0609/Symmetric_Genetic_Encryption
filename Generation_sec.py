42bdd414505743977f7b58ff823d4ac50eee2bac
Kedar Salunkhe <kedarsalunkhe325@gmail.com>
2024-03-24 16:46:15 +0530

    Apply formatting

--- Generation_sec.py
+++ Generation_sec.py
@@ -1,128 +1,113 @@
 import random
 from collections import deque
+
 from getting_data import *
 
-class Encryption(Data_Prep):
-     
 
-    def __init__(self,file:str):
+class Encryption(Data_Prep):
+
+    def __init__(self, file: str):
         super().__init__(file)
-        self.break_to_16() 
+        self.break_to_16()
         self.cross_over_points = None
         self.mutations = None
         self.permutation_factor = None
         self.private_random_num = None
-        self.params = None 
+        self.params = None
 
     def generate_params(self):
         cross_temp = []
-        mutation_temp = [] 
-        while len(cross_temp)!= 4:
-             
-            n = random.randint(0,15)
+        mutation_temp = []
+        while len(cross_temp) != 4:
+
+            n = random.randint(0, 15)
             if n not in cross_temp:
 
                 cross_temp.append(n)
         cross_temp.sort()
-        while len(mutation_temp)!= 3:
-            r = random.randint(0,15)
+        while len(mutation_temp) != 3:
+            r = random.randint(0, 15)
             if r not in mutation_temp:
                 mutation_temp.append(r)
-        
-
 
         self.cross_over_points = cross_temp
-        self.mutations = mutation_temp 
-        self.permutation_factor = random.randint(1,8)
-        self.private_random_num = random.randint(0,15)
-    
-    def gen_algo(self,ind,cross_over_points,mutations):
-        
-        #print(self.cross_over_points) 
-        
-        
-        for i in range(cross_over_points[0]+1,cross_over_points[1]+1):
-                     #print(i) 
-                     c = self.parts_16[ind][i] 
-                     self.parts_16[ind][i] = self.parts_16[ind+1][i]
-                     self.parts_16[ind+1][i] = c
-        for j in range(cross_over_points[2]+1,cross_over_points[3]+1):
-                     c1 = self.parts_16[ind][j]
-                     self.parts_16[ind][j] = self.parts_16[ind+1][j]
-                     self.parts_16[ind+1][j] = c1
+        self.mutations = mutation_temp
+        self.permutation_factor = random.randint(1, 8)
+        self.private_random_num = random.randint(0, 15)
+
+    def gen_algo(self, ind, cross_over_points, mutations):
+
+        # print(self.cross_over_points)
+
+        for i in range(cross_over_points[0] + 1, cross_over_points[1] + 1):
+            # print(i)
+            c = self.parts_16[ind][i]
+            self.parts_16[ind][i] = self.parts_16[ind + 1][i]
+            self.parts_16[ind + 1][i] = c
+        for j in range(cross_over_points[2] + 1, cross_over_points[3] + 1):
+            c1 = self.parts_16[ind][j]
+            self.parts_16[ind][j] = self.parts_16[ind + 1][j]
+            self.parts_16[ind + 1][j] = c1
         for mu in mutations:
-                     self.parts_16[ind][mu] = 128 - self.parts_16[ind][mu]
-                     self.parts_16[ind+1][mu] = 128 - self.parts_16[ind+1][mu]
-
-
+            self.parts_16[ind][mu] = 128 - self.parts_16[ind][mu]
+            self.parts_16[ind + 1][mu] = 128 - self.parts_16[ind + 1][mu]
 
     def public_key_gen(self):
         self.generate_params()
-        f_op = open("public_key.txt",'w')
+        f_op = open("public_key.txt", "w")
         f_op.write(str(self.cross_over_points))
         f_op.write("\n")
         f_op.write(str(self.mutations))
-        f_op.write("\n") 
+        f_op.write("\n")
         f_op.write(str(self.permutation_factor))
-        f_op.write("\n") 
+        f_op.write("\n")
         f_op.write(str(self.private_random_num))
         f_op.close()
-    
 
     def private_key_gen(self):
-        self.params = self.cross_over_points + self.mutations + self.permutation_factor + self.private_random_num 
+        self.params = (
+            self.cross_over_points
+            + self.mutations
+            + self.permutation_factor
+            + self.private_random_num
+        )
 
-    #     pass 
+    #     pass
     #    params = self.cross_over_points + self.mutations + [self.permutation_factor] + [self.private_random_num]
     #    params = sum(params,[])
-
-   
-        
-    
-
 
     def encryption(self):
         per = self.permutation_factor
         params = self.cross_over_points + self.mutations
-        for ind in range(0,len(self.parts_16),2):
-            if ind ==0:
-                self.gen_algo(ind,self.cross_over_points,self.mutations)
+        for ind in range(0, len(self.parts_16), 2):
+            if ind == 0:
+                self.gen_algo(ind, self.cross_over_points, self.mutations)
             else:
                 params = deque(params)
-                params.rotate(-1*per)
+                params.rotate(-1 * per)
                 params = list(params)
                 cross_temp = []
                 mutation_temp = []
                 c = 0
 
                 for i in params:
-                    if c<4:
+                    if c < 4:
                         cross_temp.append(i)
-                    elif c>3 and c<7:
+                    elif c > 3 and c < 7:
                         mutation_temp.append(i)
-                
-                self.gen_algo(ind,cross_temp,mutation_temp)
-            f = open("encrypt.txt",'wb')
-            bytea = sum(self.parts_16,[])
+
+                self.gen_algo(ind, cross_temp, mutation_temp)
+            f = open("encrypt.txt", "wb")
+            bytea = sum(self.parts_16, [])
             bytess = bytes(bytea)
             f.write(bytess)
             f.close()
-    
-
-    
 
 
-
-                
-
-         
-        
 if __name__ == "__main__":
-        obj = Encryption(file ="./getting_data.py")
-        #obj.generate_params()
-        obj.public_key_gen()
-        obj.encryption()
-        #obj.encryption()
-        #obj.cross_over()
-        
-
+    obj = Encryption(file="./getting_data.py")
+    # obj.generate_params()
+    obj.public_key_gen()
+    obj.encryption()
+    # obj.encryption()
+    # obj.cross_over()
--- getting_data.py
+++ getting_data.py
@@ -1,34 +1,33 @@
 import random
+
+
 class Data_Prep:
-    def __init__(self,file_name:str):
+    def __init__(self, file_name: str):
         self.file_name = file_name
         self.parts_16 = None
 
     def break_to_16(self):
-        f_po = open(self.file_name,'rb')
+        f_po = open(self.file_name, "rb")
         l_16 = []
         __ = []
         flag = 0
         for i in f_po.read():
-           if flag>=16:
-               flag = 0
-               l_16.append(__)
-               __ = []
-           __.append(i)
-           flag +=1
-        if len(l_16)%2 !=0:
-            l_16.append([0]*16)
-#l_16.append([random.randint(0,127)]*16)
+            if flag >= 16:
+                flag = 0
+                l_16.append(__)
+                __ = []
+            __.append(i)
+            flag += 1
+        if len(l_16) % 2 != 0:
+            l_16.append([0] * 16)
+        # l_16.append([random.randint(0,127)]*16)
         if len(l_16[-1]) != 16:
-            l_16 = l_16 + [0]*(len(16 - l_16[0]))
-#[random.randint(0,127)]*(len(16 - l_16[0]))
-
-
-
+            l_16 = l_16 + [0] * (len(16 - l_16[0]))
+        # [random.randint(0,127)]*(len(16 - l_16[0]))
 
         self.parts_16 = l_16
         return self.parts_16
-        
+
 
 if __name__ == "__main__":
     obj = Data_Prep(file_name="./getting_data.py")
